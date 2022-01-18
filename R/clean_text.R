ac744d23c1d918f6d95f74f73d3c88f4caeae923
Artan Zandian <74034506+artanzand@users.noreply.github.com>
2022-01-17 19:51:46 -0800

    Add function docstring

--- R/clean_text.R
+++ R/clean_text.R
@@ -1,1 +1,11 @@
-# A function to clean the text
+#' Cleans the text by removing special characters, html_tags, #tags, contaction words.
+#'
+#' @param text A text to clean.
+#'
+#' @return Cleaned text
+#' @export
+#'
+#' @examples
+clean_text <- function(text) {
+
+}

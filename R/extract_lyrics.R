c632b8cd92c770455e95d9562d7e0633a994d3e5
Macy Chan <macychan@student.ubc.ca>
2022-01-18 02:42:54 -0500

    Add extract_lyrics function

--- R/extract_lyrics.R
+++ R/extract_lyrics.R
@@ -1,13 +1,29 @@
 #' Extracting lyrics for a song
 #'
-#' @param token Title of the song
-#' @param song_title Artist of the song
-#' @param artist A token from Genius for asccessing lyrics
+#' @param song_title Title of the song
+#' @param artist Artist of the song
 #'
-#' @return songs Return lyrics of song with three columns: song_title, artist and lyrics
+#' @return Return song lyrics
 #' @export
 #'
 #' @examples
-extract_lyrics <- function(token, song_title, artist){
-
+#' extract_lyrics( "22", "Taylor Swift")
+extract_lyrics <- function(song_title, artist) {
+    lyrics = ""
+    url = paste0(
+        "https://genius.com/",
+        stringr::str_replace_all(artist, " ", "-"),
+        "-",
+        stringr::str_replace_all(song_title, " ", "-"),
+        "-lyrics"
+    )
+    Scraped_webpage <- rvest::read_html(url)
+    lyrics_html <- rvest::html_nodes(Scraped_webpage, '.lgZgEN')
+    stripped_htmltext <- rvest::html_text(lyrics_html)
+    vec_lyric <-
+        gsub("([a-z])([A-Z])", "\\1 \\2", stripped_htmltext)
+    for (i in vec_lyric) {
+        lyrics = paste0(lyrics , i)
+    }
+    lyrics
 }

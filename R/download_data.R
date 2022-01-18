d24e000ef05fdd53e21153888267a62b242883ee
Artan Zandian <74034506+artanzand@users.noreply.github.com>
2022-01-17 19:52:31 -0800

    Add download_data.R

---
+++ R/download_data.R
@@ -0,0 +1,13 @@
+#' Downloads dataset from kaggle to filepath and creates a dataframe with input columns
+#'
+#' @param dataset kaggle dataset name to download
+#' @param file_path location to save the file
+#' @param columns list of columns to create a dataframe
+#'
+#' @return A dataframe with the given column names
+#' @export
+#'
+#' @examples
+download_data <- function(dataset, file_path, columns){
+
+}

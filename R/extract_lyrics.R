87d5de5612f984ab7ff60a3f704e302c770f7d6d
Artan Zandian <74034506+artanzand@users.noreply.github.com>
2022-01-17 20:03:40 -0800

    Add function docstring

--- R/extract_lyrics.R
+++ R/extract_lyrics.R
@@ -1,1 +1,13 @@
-# A function to extract lyrics
+#' Extracting lyrics for a song
+#'
+#' @param token Title of the song
+#' @param song_title Artist of the song
+#' @param artist A token from Genius for asccessing lyrics
+#'
+#' @return songs Return lyrics of song with three columns: song_title, artist and lyrics
+#' @export
+#'
+#' @examples
+extract_lyrics <- function(token, song_title, artist){
+
+}

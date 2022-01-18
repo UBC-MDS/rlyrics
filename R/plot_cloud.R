48e76cfcb31c662487c77eea5e4a11c47f5e55c7
Artan Zandian <74034506+artanzand@users.noreply.github.com>
2022-01-17 19:51:56 -0800

    Add function docstring

--- R/plot_cloud.R
+++ R/plot_cloud.R
@@ -1,1 +1,23 @@
-# A function to create a wordcloud
+#' Creates a wordcloud of most occuring words in a string or list of strings
+#'
+#' @param song with artist as dictionary key and song_title as value. Both key and value are strings.
+#' @param file_path The location to save the file
+#' @param max_font_size maximum font size
+#' @param max_words maximum number of words to be included in wordcloud
+#' @param background_color background color
+#' @param show whether to display the plot to screen
+#'
+#' @return A wordcloud image
+#' @export
+#'
+#' @examples
+plot_cloud <- function(
+    song,
+    file_path,
+    max_font_size=30,
+    max_words=120,
+    background_color="black",
+    show=False,
+){
+
+}

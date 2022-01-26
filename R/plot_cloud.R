fb18b61afc1710e90afc86d5d8313f19508a4e4f
Artan Zandian <74034506+artanzand@users.noreply.github.com>
2022-01-25 19:25:30 -0800

    Add plot_cloud.R

--- R/plot_cloud.R
+++ R/plot_cloud.R
@@ -1,8 +1,7 @@
-#' Creates a wordcloud of most occuring words in a string or list of strings
+#' Creates a wordcloud of most occurring words in a string or list of strings
 #'
-#' @param song with artist as dictionary key and song_title as value. Both key and value are strings.
-#' @param file_path The location to save the file
-#' @param max_font_size maximum font size
+#' @param song A data frame including song_title and artist in each column
+#' @param max_font_size maximum font size - numeric
 #' @param max_words maximum number of words to be included in wordcloud
 #' @param background_color background color
 #'
@@ -10,12 +9,56 @@
 #' @export
 #'
 #' @examples
-plot_cloud <- function(
-    song,
-    file_path,
-    max_font_size=30,
-    max_words=120,
-    background_color="black"){
+#'song <- data.frame(song_title  = c("22", "Bohemian Rhapsody"), artist = c("Taylor Swift", "Queen"))
+#'plot_cloud(song, max_font_size=1.6, max_words=100, background_color="white")
 
+library(wordcloud)
+source("R/extract_lyrics.R")
+source("R/clean_text.R")
+
+
+plot_cloud <- function(song, max_font_size=1, max_words=100, background_color="black"){
+
+    # check input types
+    if(!is.data.frame(song)){
+        stop("song should be of type dataframe")
+    }
+
+    if(ncol(song) != 2){
+        stop("song only accepts a dataframe with two columns")
+    }
+
+    if(!is.character(background_color)){
+        stop("max_font_size should be of type character")
+    }
+
+    if(!is.numeric(max_font_size) & !is.numeric(max_words)){
+        stop("Both max_font_size and max_words should be of type numeric.")
+    }
+
+
+    text = ""
+    # Create a string of all song lyrics
+    for (i in seq_along(song)){
+        song_title <- song[[i, 1]]
+        artist <- song[[i, 2]]
+        raw_lyrics <- extract_lyrics(song_title, artist)
+        clean_lyrics <- clean_text(text=raw_lyrics)
+        text <- paste(text, clean_lyrics)
+    }
+
+    # Create word frequency matrix
+    corpus <- tm::Corpus(tm::VectorSource(text))
+    dtm <- tm::TermDocumentMatrix(corpus)
+    matrix <- as.matrix(dtm)
+    words <- sort(rowSums(matrix),decreasing=TRUE)
+    df <- data.frame(word = names(words), freq=words)
+
+    # Filter max_words
+    max_words <- min(max_words, nrow(df))
+    df <- df |> dplyr::slice(1:max_words)
+
+    # Generate the wordcloud
+    wordcloud2::wordcloud2(data=df, size=max_font_size, backgroundColor=background_color)
 }
 

2765cfccc5a6621ca0f31ba0952d52c23a5d6f4b
manju-abhinandana <manju.na87@gmail.com>
2022-01-28 12:06:46 -0800

    Removed filepath function and test

--- tests/testthat/test-download_data.R
+++ tests/testthat/test-download_data.R
@@ -1,41 +1,33 @@
 
 dataset <- "geomack/spotifyclassification"
-file_path <- "data/spotify"
 columns <- c("song_title", "artist")
 
 
 # Testing input types
 test_that("dataset name should be of type string", {
-    expect_error(download_data(1, file_path, columns),
+    expect_error(download_data(1, columns),
                  "dataset name should be of type string")
 })
 
-test_that("File_path  should be of type string", {
-    expect_error(download_data(dataset, 1, columns),
-                 "File_path  should be of type string")
-})
-
 test_that("The columns should be of type string", {
-    expect_error(download_data(dataset, file_path, 1),
+    expect_error(download_data(dataset, 1),
                  "The columns should be of type string")
 })
 
 test_that("Two columns should be retrieved", {
-    expect_error(download_data(dataset, file_path , c("song_title")),
+    expect_error(download_data(dataset , c("song_title")),
                  "Two columns should be retrieved")
 })
 
 test_that("Incorrect column names, please check again", {
-    expect_error(download_data(dataset, file_path , c("song_title", "test")),
+    expect_error(download_data(dataset , c("song_title", "test")),
                  "Incorrect column names, please check again")
 })
 
 test_that("hpapy case", {
-    target <- read.csv(here::here(file_path, "data.csv"))
-    output <- download_data(dataset, file_path, c("song_title", "artist"))
+    target <- read.csv ("data.csv")
+    output <- download_data(dataset, c("song_title", "artist"))
     expect_equal(target[,c('song_title', 'artist')], output)
 })
-
-
 
 

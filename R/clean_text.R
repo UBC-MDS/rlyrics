5887ee14c8739f024de182a21bf661f921cddea3
Abhiket Gaurav <p12abhiketg@iimidr.ac.in>
2022-01-27 16:00:04 -0800

    updated clean_text and test

--- R/clean_text.R
+++ R/clean_text.R
@@ -1,13 +1,50 @@
-#' Cleans the text by removing special characters, html_tags, #tags, contaction words and convert everything to lower case.
+#' Cleans the text by removing special characters, html_tags, #tags, contaction words, stop-words and convert everything to lower case.
 #'
 #' @param text A text to clean.
-#' @param bool_contra_dict A flag to clear contraction words.
 #' @return text Cleaned text
 #' @export
 #'
 #' @examples
 #' text <- "Early optimization is the root of all evil!"
-#' clean_text(text,bool_contra_dict= False)
+#' clean_text(text)
 #> [1] "early optimization is the root of all evil"
-clean_text <- function(text,True) {
+
+install.packages("stringr")
+install.packages("textclean")
+install.packages("tm")
+
+library(stringr)
+library(textclean)
+library(tm)
+
+clean_text <- function(text) {
+
+    # check input types
+    if(!is.character(text)){
+        stop("Text should be a variable of type string.")
+    }
+
+    # check for blank string
+    if(str_length(trimws(text)) == 0 ){
+        stop("Blank text input")
+    }
+    
+    # check if the text is encoded properly
+    if(validUTF8(text) != TRUE){
+        stop("please input a proper text")
+    }
+    
+    # Lowercase
+    temp <- tolower(text)
+    # replacing contraction words
+    temp <- replace_contraction(temp)
+    # removing punctuation marks
+    temp <- gsub('[[:punct:]]', '', temp)
+    # removing stopwords special charaters
+    temp <- removeWords(temp,stopwords("en"))
+    # removing special charaters
+    temp <- stringr::str_replace_all(temp,"[^a-zA-Z\\s]", " ")
+    # removing extra spaces
+    temp <- stringr::str_replace_all(temp,"[\\s]+", " ")
+    return(temp)
 }
--- tests/testthat/test-clean_text.R
+++ tests/testthat/test-clean_text.R
@@ -1,3 +1,21 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+# Testing output/input is correct
+lyrics <- "Early optimization is the root of all evil!"
+test_that("output is string", {
+    expect_equal(is.character(clean_text(lyrics)), TRUE)
 })
+
+lyrics = " "
+test_that("empty dataframe", {
+    expect_warning(clean_text(lyrics))
+})
+
+lyrics = 22
+test_that("wrong input type for lyrics", {
+    expect_warning(clean_text(lyrics))
+})
+
+lyrics = "asdllll³³œ¿¿¿"
+test_that("Non UTF8 Character", {
+    expect_warning(clean_text(lyrics))
+})
+
